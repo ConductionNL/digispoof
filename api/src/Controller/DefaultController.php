50972e96f63d7bf0efda17614b7f6c71d96e585f
Ruben van der Linde <ruben@conduction.nl>
2020-03-04 11:41:17 +0100

    Bug fix on array notation

--- api/src/Controller/DefaultController.php
+++ api/src/Controller/DefaultController.php
@@ -1,41 +1,51 @@
 <?php
-
-// src/Controller/DefaultController.php
-
+// src/Controller/LuckyController.php
 namespace App\Controller;
 
-use Doctrine\ORM\EntityManagerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Component\Routing\Annotation\Route;
 
-use App\Service\CommonGroundService; 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Session\Session;
 
+use App\Service\SjabloonService;
+use App\Service\BRPService;
+use App\Service\RequestTypeService;
+use App\Service\ContactService;
+use App\Service\AssentService;
+
+use App\Service\CommonGroundService;
+use App\Service\RequestService;
+use App\Service\ApplicationService;
+
+/**
+ */
 class DefaultController extends AbstractController
 {
-    /**
-     * @Route("/")
-     * @Template
-     */
+	/**
+	 * @Route("/")
+	 * @Template
+	 */
 	public function viewAction(Request $request, CommonGroundService $commonGroundService)
-    {    	
-    	$token = $request->query->get('token');
-    	$responceUrl = $request->query->get('responceUrl');
-    	$brpUrl = $request->query->get('brpUrl');
-    	$url = $request->getHost();
-    	
-    	if(!$brpUrl){
-    		$brpUrl = str_replace(['ds','digispoof'].'.','brp.',$url);
-    	}
-    	
-    	if($brpUrl == 'localhost'){
-    		$brpUrl = "https://brp.dev.huwelijksplanner.online";
-    	}
-    	
-    	$people = $commonGroundService->getResourceList($brpUrl.'/ingeschrevenpersonen');
-    	
-    	    	
-    	return ['people'=>$people, 'responceUrl' => $responceUrl, 'token' => $token];
-    }
+	{
+		$token = $request->query->get('token');
+		$responceUrl = $request->query->get('responceUrl');
+		$brpUrl = $request->query->get('brpUrl');
+		$url = $request->getHost();
+		
+		if(!$brpUrl){
+			$brpUrl = str_replace(['ds.','digispoof.'],'brp.',$url);
+		}
+		
+		if($brpUrl == 'localhost'){
+			$brpUrl = "https://brp.dev.huwelijksplanner.online";
+		}
+		$people = $commonGroundService->getResourceList($brpUrl.'/ingeschrevenpersonen');
+		
+		
+		return ['people'=>$people, 'responceUrl' => $responceUrl, 'token' => $token];
+	}
 }
